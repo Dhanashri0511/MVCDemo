018510c967b5b3053241bee68bb4d65004cf15d3
Dhanashri0511 <shindedhanashri194@gmail.com>
2023-03-13 13:38:30 +0530

    initial comit

---
+++ MVCDemo/Models/user.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class user
+    {
+        public string name { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
